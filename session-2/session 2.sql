c25bb38e3ca45b6836e065aa717973ebfb6df0ae
parth <109512441+Parth3030@users.noreply.github.com>
2026-07-09 16:08:14 +0530

    Add files via upload

---
+++ session-2/session 2.sql
@@ -0,0 +1,18 @@
+-- TASK 1 --
+
+SELECT * FROM restaurants;
+
+-- TASK 2 -- 
+
+SELECT name, rating
+FROM zomato_reviews;
+
+-- TASK 3 -- 
+
+SELECT movie_name AS Title, release_year AS 'Year Released' 
+FROM movies;
+
+-- TASK 4 -- 
+
+-- This query selects all rows and columns from the products table
+SELECT * FROM products;
